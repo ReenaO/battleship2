4d2b567c8942e55f8cca6bb06a9f936d4a5230c2
ReenaO <reena.mccarthy@hotmail.com>
2021-12-29 00:04:12 +0000

    started add javascript ( start button)

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        if(this.getAttribute("data-type") === "start") {
+            alert("Start Game"); 
+            }
+        } 
+            
+        }
+
+        )
+

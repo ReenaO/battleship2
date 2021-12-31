890073167dd5938ba34009ea5d268faf024b0a45
ReenaO <reena.mccarthy@hotmail.com>
2021-12-31 09:03:12 +0000

    styling to buttons and grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button"); {
 
     for (let button of buttons) {
-        if(this.getAttribute("data-type") === "start") {
-            alert("Start Game"); 
-            } 
-        } 
-            
-        }
+      if(this.getAttribute("data-type") === "start") {
 
-        )
+            let(this.getAttribute("data-type") === "New Game");
+            alert("Start Game"); 
+
+            }  else { 
+                let(this.getAttribute("data-type") ==="Continue") 
+                    akert("Continue Game");
+            } 
+        }
+    }
+}
+                
+            
+                
+ 
+            
+        
+            
+        
+
+        
 
 
         function runGame() {
@@ -32,4 +46,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
        }
 
-        
+    
